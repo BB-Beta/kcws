15d8c69482f507be3f3be52b63979a7094ec2da4
zhangchen <Zhangchen@ling.ai>
2017-07-06 18:27:43 +0800

    add test case generator

---
+++ kcws/train/generate_testcases.py
@@ -0,0 +1,31 @@
+#coding=utf8
+'''
+Created on 2017年7月6日
+
+@author: wuling
+'''
+
+def loadFile(path, result):
+    with open(path, 'r') as f_input:
+        for line in f_input:
+            attrs = line.strip().split('\t')
+            result[attrs[1]] = attrs[0]
+    
+def process():
+    w2v_dict, label_dict = dict(), dict()
+    loadFile('kcws/models/pos_vocab.txt', label_dict)
+    loadFile('kcws/models/word_vocab.txt.txt', w2v_dict)
+    with open('test.txt', 'r') as f_input:
+        with open('result.txt', 'w') as f_output:
+            for line in f_input:
+                elems = line.strip().split('')
+                words = elems[:50]
+                labels = elems[250:]
+                for word in words:
+                    f_output.write('%s\t'%(w2v_dict[elem]))
+                f_output.write('\n')
+                for elem in labels:
+                    f_output.write('%s\t'%(label_dict[elem]))
+    
+if __name__ == '__main__':
+    process()
